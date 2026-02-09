46ed6136b0ca2d918797dcdfc140872d3c31c722
Ubuntu <ubuntu@ip-172-31-29-154.ap-south-1.compute.internal>
2026-02-09 08:04:33 +0000

    first commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from "@prisma/client";
 const prismaClientSingleton = () => {
   return new PrismaClient({
     log: ["error"],
+
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 };
 
@@ -12,8 +18,12 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const prisma =
+  globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// Only cache in development (avoid hot reload leak)
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
